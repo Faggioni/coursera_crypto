533763b46f53b60e033af31a74b2253bf78e464e
Faggioni <miguelfaggioni@gmail.com>
2016-08-25 11:01:41 -0400

    Working Thought

--- 3rd Week/crypto.py
+++ 3rd Week/crypto.py
@@ -1,65 +1,16 @@
-from Crypto.Hash import SHA256 as sha
-import os
-
-h = sha.new()
-h.update('hello')
-print h.hexdigest
-
-
-statinfo = os.stat('test.mp4')
-f = open('test.mp4','r')
-print statinfo.st_size / 1024.0
-print f
-f.close()
-
-
-# define the function to split the file into smaller chunks
-def splitFile(inputFile,chunkSize):
-    #read the contents of the file
-    f = open(inputFile, 'rb')
-    data = f.read() # read the entire content of the file
+def split_file(name,size):
+    f = open(name,'rb')
+    video = f.read()
     f.close()
-
-    # get the length of data, ie size of the input file in bytes
-    bytes = len(data)
-
-    #calculate the number of chunks to be created
-    noOfChunks= bytes/chunkSize
-    if(bytes%chunkSize):
-        noOfChunks+=1
-
-    #create a info.txt file for writing metadata
-    f = open('info.txt', 'w')
-    f.write(inputFile+','+'chunk,'+str(noOfChunks)+','+str(chunkSize))
-    f.close()
-
-    chunkNames = []
-    for i in range(0, bytes+1, chunkSize):
-        fn1 = "chunk%s" % i
-        chunkNames.append(fn1)
-        f = open(fn1, 'wb')
-        f.write(data[i:i+ chunkSize])
-        f.close()
+    data = []
+    for i in range(0,len(video),size):
+        if ((i + size - 1) < len(video)):
+            data.append(video[i:i+size-1])
+        else:
+            data.append(video[i:])
+    return data
 
 
 
-#define the function to join the chunks of files into a single file
-def joinFiles(fileName,noOfChunks,chunkSize):
-    dataList = []
-    for i in range(0,noOfChunks,1):
-        chunkNum=i * chunkSize
-        chunkName = fileName+'%s'%chunkNum
-        f = open(chunkName, 'rb')
-        dataList.append(f.read())
-        f.close()
-
-    f = open(fileName, 'wb')
-    for data in dataList:
-        f.write(data)
-        f.close()
 
 
-# call the file splitting function
-splitFile('test.mp4',110000000)
-#call the function to join the splitted files
-joinFiles('chunk.mp4',7,110000000)
--- 3rd Week/main.py
+++ 3rd Week/main.py
@@ -1,5 +1,20 @@
 import crypto as s
+from Crypto.Hash import SHA256 as sha
 """
 Test:
 03c08f4ee0b576fe319338139c045c89c3e8e9409633bea29442e21425006ea8
 """
+#OJO HEX DECODE
+messages = s.split_file('test.mp4',1024)
+h=[]
+n = len(messages)
+ht = sha.new()
+for i in range(len(messages)):
+    if ((n-i) == n):
+       ht.update(messages[n-1])
+       h.append(ht.digest())
+    else:
+        ht.update(messages[n-i-1]+h[i-1])
+        h.append(ht.digest())
+print len(h)
+print h[len(h)-1].encode('hex')
