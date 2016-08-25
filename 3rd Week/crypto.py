fa6fcf4a977329db6ab113e76e84ef9645c72902
Faggioni <miguelfaggioni@gmail.com>
2016-08-25 15:30:58 -0400

    Solution Week 3

--- 3rd Week/crypto.py
+++ 3rd Week/crypto.py
@@ -1,16 +1,27 @@
+import sys
 def split_file(name,size):
     f = open(name,'rb')
     video = f.read()
     f.close()
     data = []
     for i in range(0,len(video),size):
-        if ((i + size - 1) < len(video)):
-            data.append(video[i:i+size-1])
+        if ((i + size) < len(video)):
+            data.append(video[i:i+size])
         else:
             data.append(video[i:])
     return data
 
-
-
+def splitting(name,size):
+    f = open(name,'rb')
+    video = f.read()
+    f.close()
+    data = []
+    string =''
+    for byte in video:
+        string += byte
+        if (sys.getsizeof(string) == 1024):
+            data.append(string)
+            string=''
+    return data
 
 
--- 3rd Week/main.py
+++ 3rd Week/main.py
@@ -1,20 +1,22 @@
 import crypto as s
 from Crypto.Hash import SHA256 as sha
+import sys
 """
 Test:
 03c08f4ee0b576fe319338139c045c89c3e8e9409633bea29442e21425006ea8
 """
 #OJO HEX DECODE
-messages = s.split_file('test.mp4',1024)
+messages = s.splitting('6.1.intro.mp4',1024)
 h=[]
-n = len(messages)
-ht = sha.new()
-for i in range(len(messages)):
+n = len(messages)-1
+print sys.getsizeof(messages[0])
+for i in range(n+1):
     if ((n-i) == n):
-       ht.update(messages[n-1])
-       h.append(ht.digest())
-    else:
-        ht.update(messages[n-i-1]+h[i-1])
+        ht = sha.new()
+        ht.update(messages[n])
         h.append(ht.digest())
-print len(h)
+    else:
+        ht = sha.new()
+        ht.update(messages[n-i]+h[i-1])
+        h.append(ht.digest())
 print h[len(h)-1].encode('hex')
