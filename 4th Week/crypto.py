8b2fc9ea4f4052d2a55d8347758b4828094b62cb
Faggioni <miguelfaggioni@gmail.com>
2016-09-04 20:09:48 -0400

    Week 4

---
+++ 4th Week/crypto.py
@@ -0,0 +1,14 @@
+import urllib2
+import sys
+
+
+class Crypto(PaddingOracle):
+
+    def strxor(a, b):
+        if len(a) > len(b):
+            return "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(a[:len(b)], b)])
+        else:
+            return "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(a, b[:len(a)])])
+
+
+
