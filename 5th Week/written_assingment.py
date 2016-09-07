1f5e761939d757ac9274a418177877481b1fdf03
Faggioni <miguelfaggioni@gmail.com>
2016-09-07 15:38:09 -0400

    Written Assigment

--- 5th Week/written_assingment.py
+++ 5th Week/written_assingment.py
@@ -2,15 +2,24 @@
 """
 	7a + 23b = 1
 """
+def test():
+	for i in range(20):
+		for j in range(20):
+			if (7*i + 23*j == 1):
+				a = i
+				b = j
+				break
 
-for i in range(20):
-	for j in range(20):
-		if (7*i + 23*j == 1):
-			a = i
-			b = j
-			break
+	print a,b
 
-print a,b
+def xgcd(a,b):
+	prevx, x = 1, 0; prevy, y = 0, 1
+	while b:
+	q = a/b
+	x, prevx = prevx - q*x, x
+	y, prevy = prevy - q*y, y
+	a, b = b, a % b
+	return a, prevx, prevy
 
 
 
