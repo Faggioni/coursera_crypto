518b5bff37613e9eb54177798a2820299e0e7d4e
Faggioni <miguelfaggioni@gmail.com>
2016-09-07 22:38:37 -0400

    Adding Some Documentation

---
+++ 5th Week/assingment.py
@@ -0,0 +1,2 @@
+import numtools as n
+
--- 5th Week/numtools.py
+++ 5th Week/numtools.py
@@ -1,5 +1,13 @@
 import math 
 
+"""eu_ext			
+	Euclid Extended Algorith
+	@args b: Integer
+		  n: Integer
+	@return : gdc(b,n)
+			  x and y integer	
+			from bx + ny = gdf(b,n)
+"""
 def eu_ext(b, n):
 	x0, x1, y0, y1 = 1, 0, 0, 1
 	while n != 0:
@@ -8,6 +16,12 @@ def eu_ext(b, n):
 		y0, y1 = y1, y0 -q * y1
 	return b, x0, y0
 
+
+"""gdc
+	Greatest Common Divisor
+	@args: a,b Integer to evaluate
+	@return gdc(a,b)
+"""
 def gdc(a, b):
 	mini = min(a,b)
 	gcd = 1 
@@ -17,6 +31,11 @@ def gdc(a, b):
 			gcd = i
 	return gcd
 
+"""Zinv
+	Calculates the Zn* set
+	@args: n Integer 
+	@return l - list set	
+"""		
 def Zinv(n):
 	l = []
 	iter = range(36)[1:]
@@ -25,13 +44,25 @@ def Zinv(n):
 			l.append(i)
 	return l
 
-def sqroot(n,r,m):
+"""sqroot
+	Calculate square root of n module m
+	@args: n argument of the sqrt function
+		   m Zm
+	@return x integer 
+"""
+def sqroot(n,m):
 	for i in range(1000):
 		x = math.sqrt(m*i+n)
 		if (math.floor(x) == x):	
 			break
 	return x
 
+"""Dlog2table
+	Discrete Logarith table from small Zn
+	@args: base - base of the log
+		   n - Zn
+	@return: list - table
+"""
 def Dlog2table(base,n):
 	iter = range(n)[1:]
 	table = []
@@ -42,6 +73,13 @@ def Dlog2table(base,n):
 				break
 	return table
 
+
+"""order
+	Calcules the order of a element in a cyclid group Zn
+	@args: num - integer belonging to Zn
+		   mod - Zn
+	@return: o(num) in Zmod
+"""
 def order(num,mod):
 	counter=1
 	iter = range(mod+1)[1:]
